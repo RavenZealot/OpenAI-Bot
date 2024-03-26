e1883ed1daf970a1a74d121c99349f20422a01db
RavenZealot <netzakuro@gmail.com>
2024-03-27 00:08:12 +0900

    :bug: fix / Fixed timing of log rotation execution

--- bot/index.js
+++ bot/index.js
@@ -10,6 +10,9 @@ const PATH = require('path');
 const logger = require('../utils/logger');
 const messenger = require('../utils/messenger');
 
+// ログファイルのバックアップと新規作成
+logger.logRotate();
+
 // Discord クライアントを作成
 const DISCORD = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds] });
 
@@ -38,9 +41,6 @@ DISCORD.once('ready', async () => {
         data.push(commands[commandName].data);
     }
     await DISCORD.application.commands.set(data);
-
-    // ログファイルのバックアップと新規作成
-    logger.logRotate();
 
     logger.logToFile(`${DISCORD.user.tag} でログインしました`);
 });
